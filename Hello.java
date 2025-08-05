71af02519d6bd63bed30edf1414cee1370db67b7
GunothamE <edem.gotham@gmail.com>
2025-08-05 22:38:27 +0530

    initial commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello{
+    void print(){
+        System.out.println("Hello! This is my first tns Java program.");
+    }
+}
+class HelloMain{
+    public static void main(String args[]){
+        Hello obj = new Hello();
+        obj.print();
+    }
+}
